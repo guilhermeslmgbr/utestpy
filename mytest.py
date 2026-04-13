52602a9a3d5eeb9f112560f479306c2d0244ac51
Guilherme Camilo <82889569+guilhermeslmgbr@users.noreply.github.com>
2026-04-13 23:44:11 +0000

    7. Executando Todos os Testes

--- mytest.py
+++ mytest.py
@@ -27,9 +27,16 @@ class MyTest(TestCase):
         print("tear_down")
 
 
-result = TestResult()
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
+
 runner = TestRunner()
 runner.run(suite)
 
